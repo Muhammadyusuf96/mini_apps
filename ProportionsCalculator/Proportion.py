fd2a4e965997e073fa22f458ac55b5899b27b7c4
Muhammadyusuf96 <36552832+Muhammadyusuf96@users.noreply.github.com>
2021-06-14 15:22:22 +0500

    initial commit
    initial commit - ProportionsCalculator

---
+++ ProportionsCalculator/Proportion.py
@@ -0,0 +1,134 @@
+from tkinter import StringVar
+from tkinter.ttk import Frame, Separator, Entry, Label
+
+
+class Main(Frame):
+    def __init__(self):
+        super().__init__()
+
+        self.master.geometry("292x104+537+300")
+        self.master.resizable(0, 0)
+        self.master.title("Proportsiya kalkulyatori")
+
+        self.TLabel1 = Label(self.master, text="Asosiy bog'liqlik")
+        self.TLabel1.place(x=101, y=6, height=19, width=90)
+
+        self.TSeparator1 = Separator(None)
+        self.TSeparator1.place(x=0, y=60, width=320)
+
+        self.entries = {}
+        self.str_var = {}
+        self.calced = ''
+
+        self.pack(fill="both", expand=True)
+        self.basic()
+        self.master.mainloop()
+
+    def calc(self, key):
+        if key != '00' and key != '10':
+            obj = self.str_var[key]
+            a = self.str_var['00'].get()
+            b = self.str_var['10'].get()
+            if b and a and obj.get():
+                val = float(obj.get())
+                if key.startswith('0'):
+                    temp = val * float(b) / float(a)
+                    name = '1' + key[1:]
+                    self.calced = self.str_var[name]._name
+                    self.str_var[name].set(temp)
+                else:
+                    temp = val * float(a) / float(b)
+                    name = '0' + key[1:]
+                    self.calced = self.str_var[name]._name
+                    self.str_var[name].set(temp)
+        else:
+            if len(self.entries) >= 4:
+                self.reset()
+            self.center_window()
+
+    def reset(self):
+        if len(self.entries) > 4:
+            keys = ['00', '01', '10', '11']
+            for key in self.str_var.copy():
+                if key not in keys:
+                    self.str_var.pop(key, None)
+                    self.entries[key].destroy()
+                    self.entries.pop(key, None)
+                elif key != '00' and key != '10':
+                    self.str_var[key].set('')
+        self.calced = ''
+
+    def on_change(self, *args):
+        name = args[0]
+        for key in self.str_var.copy():
+            obj = self.str_var.get(key, None)
+            if obj:
+                if obj._name == name:
+                    temp = self.clear(obj.get())
+                    if obj.get() != temp:
+                        obj.set(temp)
+                    if self.calced != name:
+                        self.calc(key)
+                    else:
+                        self.calced = ''
+        self.check_for_new_entries()
+
+    def clear(self, value):
+        value.replace(',', '.')
+        if value.startswith('.'):
+            value = '0' + value
+        temp = ''
+        for val in value:
+            if val.isdigit():
+                temp += val
+            elif val == '.':
+                if '.' not in temp:
+                    temp += val
+
+        if temp.startswith('0'):
+            if '.' in temp:
+                if len(temp[:temp.index('.')]) > 1:
+                    temp = str(int(temp[:temp.index('.')])) + temp[temp.index('.'):]
+            elif len(temp) > 1:
+                temp = temp[1:]
+
+        return temp
+
+    def check_for_new_entries(self):
+        t = 0
+        for key in list(self.entries.keys())[-2:]:
+            if self.str_var[key].get() != '':
+                t += 1
+        if t >= 2:
+            self.add_new_entries()
+
+    def basic(self):
+        for row in range(0, 2):
+            for col in range(0, 2):
+                cel = str(col) + str(row)
+                self.str_var[cel] = StringVar()
+                self.entries[cel] = Entry(self, textvariable=self.str_var[cel])
+                self.str_var[cel].trace("w", self.on_change)
+                self.entries[cel].grid(row=row, column=col, padx=(10, 10), pady=(26, 5))
+
+    def add_new_entries(self):
+        start = len(self.entries) // 2
+        if start <= 20:
+            for row in range(start, start + 1):
+                for col in range(0, 2):
+                    cel = str(col) + str(row)
+                    self.str_var[cel] = StringVar()
+                    self.entries[cel] = Entry(self, textvariable=self.str_var[cel])
+                    self.str_var[cel].trace("w", self.on_change)
+                    self.entries[cel].grid(row=row, column=col, padx=(10, 10), pady=(5, 5))
+            self.center_window()
+
+    def center_window(self):
+        self.master.geometry("")
+        x = (self.master.winfo_screenwidth() - self.master.winfo_width()) / 2
+        y = (self.master.winfo_screenheight() - self.master.winfo_height()) / 2 - 32
+        self.master.geometry('+%d+%d' % (x, y))
+
+
+if __name__ == '__main__':
+    app = Main()
---
+++ README.md
@@ -0,0 +1,21 @@
+# Mini projects
+
+Here I will put small projects.
+
+## List of mini projects:
+
+- [ProportionsCalculator](#ProportionsCalculator) - This is a proportions calculator.
+- [B](#b)
+- [C](#c)
+
+## ProportionsCalculator
+
+The calculator uses cross multiplication to solve. You must first enter the base ratio in the boxes above the line, then you can enter the unknown. Fields can be automatically added up to 20 when filling in the last field. All calculations will be made relative to the base ratio. All fields are reset when you change the base ratio.
+
+## B
+
+**BCD**
+
+## C
+
+**CDE**
